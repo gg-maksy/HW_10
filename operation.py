99b2c59c434fe241a69fe2d816f2e3dce4b577d8
v.dunkan <v.dunkan@gmail.com>
2023-04-04 23:44:00 +0300

    by Skiff

--- operation.py
+++ operation.py
@@ -9,21 +9,32 @@ class Field:
 
     def __str__(self) -> str:
         return str(self.value)
+    
+    def __repr__(self) -> str:
+        return str(self)
 
 
 class Record(Field):
 
-    def __init__(self, name, phone):
+    def __init__(self, name, phone=None):
         self.name = name
-        self.phone = phone
+        self.phones = [phone] if phone else []
+    
+    def add_phone(self, phone):
+        self.phones.append(phone)
+    
+    def add_phones(self, phones):
+        self.phones += phones
 
-    def change_phone(self, new_phone):
-        old_phone = self.phone
-        self.phone = new_phone
+    def change_phone(self, old_phone, new_phone):
+        for i, p in enumerate(self.phones):
+            if p.value == old_phone.value:
+                self.phones[i] = new_phone
+        # old_phone = self.phone
+        # self.phone = new_phone
         return f'Change {old_phone} to {new_phone}'
     
     
-
 class Phone(Field):
     def __init__(self, value):
         if not isinstance(value, int):
@@ -33,7 +44,6 @@ class Phone(Field):
 
 class Name(Field):
     pass
-
 
 
 class AdressBook(UserDict):
@@ -46,12 +56,24 @@ if __name__ == '__main__':
     adress_book = AdressBook()
     cont = Record(Name('Boris'), Phone(+380665411384))
 
-    cont.change_phone(+231331)
+    cont.change_phone(Phone(+380665411384), Phone(+231331))
     
     adress_book.add_contact(cont)
 
     for key, value in adress_book.items():
-        print(key, value.phone)
+        print(key, value.phones)
 
+    cont.add_phone(Phone(62743883))
+    
+    for key, value in adress_book.items():
+        print(key, value.phones)
+    
+    
+    cont.add_phones([Phone(352266), Phone(30689340986)])
+    
+    for key, value in adress_book.items():
+        print(key, value.phones)
+    
+    
 
 
